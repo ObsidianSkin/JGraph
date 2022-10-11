b4a05e07e544e1c0b0a97665952d80fe736da597
Christopher Blake Childress <cchildr3@vols.utk.edu>
2022-10-11 16:13:47 -0400

    adding maze generation

--- maze.sh
+++ maze.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-if [ $# -lt 2 ]
+if [ "$1" == "test" ]; then
+	bin/maze 1 1 | jgraph -P | ps2pdf - | convert - maze1.jpg
+	bin/maze 2 2 | jgraph -P | ps2pdf - | convert - maze2.jpg
+	bin/maze 3 3 | jgraph -P | ps2pdf - | convert - maze3.jpg
+	bin/maze 4 4 | jgraph -P | ps2pdf - | convert - maze4.jpg
+	bin/maze 5 5 | jgraph -P | ps2pdf - | convert - maze5.jpg
+elif [ $# -lt 2 ]
 	then echo "usage ./maze.sh row col"
 else
 	bin/maze $1 $2 | jgraph -P | ps2pdf - | convert - maze.jpg
--- src/maze.cpp
+++ src/maze.cpp
@@ -6,6 +6,8 @@
 
 #include <string>
 #include <cstdio>
+#include <cstdlib>
+#include <ctime> //for seeding random
 #include "disjoint_set.hpp"
 
 using namespace std;
@@ -13,22 +15,103 @@ using namespace std;
 //TODO
 //-implement disjoint sets
 //	-using plank's implementation
+//	-not actually going to use it?
 //-create maze (Random Prim's algo)
+//	-how to determine walls for size > 10?
 //-solve maze
+
+vector<string> walls; //strings of wall location (e.g., "01" = row 0 & col 1)
+
+//helper to add walls to list
+void addWalls(int max_row, int max_col, int cur_row, int cur_col) {
+	//check top
+	if(cur_row + 1 < max_row) walls.push_back(to_string(cur_row + 1) + to_string(cur_col));
+
+	//check bot
+	if(cur_row - 1 > 0) walls.push_back(to_string(cur_row - 1) + to_string(cur_col));
+	
+	//check left
+	if(cur_col - 1 > 0) walls.push_back(to_string(cur_row) + to_string(cur_col - 1));
+	
+	//check right
+	if(cur_col + 1 < max_col) walls.push_back(to_string(cur_row) + to_string(cur_col + 1));
+}
 
 //helper to make maze (Prim's algo)
 void makeMaze(int **board, int row, int col) {
 	//inital board state (0/black = spot, 1/white = wall)
-	int color = 0;
+	//int color = 0;
 	for (int i = 0; i < row; i++) { 
 		for (int j = 0; j < col; j++) {
-			if (i % 2 != 0) color = 1;
-			board[i][j] = color;
-			color ^= 1;
+			board[i][j] = 0;
 		}
 	}
 
-	//create maze
+	//get starting position (must convert back to original row/col size
+	//	+ only get even positions)
+	int start_row = 2 * (rand() % (((row - 1) / 2) + 1));
+	int start_col = 2 * (rand() % (((col - 1) / 2) + 1));
+
+	board[start_row][start_col] = 1; //mark as part of maze
+
+	addWalls(row, col, start_row, start_col); //add walls to wall list
+
+	//test
+	/*printf("start: %d %d\n", start_row, start_col);
+	for(int i = 0; i < walls.size(); i++) {
+		printf("%s\n", walls.at(i).c_str());
+	}*/
+	
+	//while still walls in list
+	while(walls.size() > 0) {
+		int rand_wall = rand() % walls.size(); //pick a random wall
+		string wall = walls.at(rand_wall);
+
+		//printf("%s ", wall.c_str());
+		//check cells on either side
+		int wall_row = wall[0] - '0';
+		int wall_col = wall[1] - '0';
+		//printf("%d %d ", wall_row, wall_col);
+		if(wall_col % 2 == 0) {
+			//if col is even, check vertical
+			//printf("horizontal\n");
+			//if only one is set
+			if(board[wall_row - 1][wall_col] ^ board[wall_row + 1][wall_col]) {
+				board[wall_row][wall_col] = 1; //mark wall as visited
+				
+				//mark adjacent node as visited plus add neighboring walls to list
+				if(!(board[wall_row - 1][wall_col])) { //check left
+					board[wall_row - 1][wall_col] = 1; 
+					addWalls(row, col, wall_row - 1, wall_col);
+				} else { //check right
+					board[wall_row + 1][wall_col] = 1;
+					addWalls(row, col, wall_row + 1, wall_col);
+				}
+			} //else printf("double horizontal\n");
+		} else if(wall_row % 2 == 0) {
+			//if row is even, then check horizontal
+			//printf("verical\n");
+			//if only one is set
+			if(board[wall_row][wall_col - 1] ^ board[wall_row][wall_col + 1]) {
+				board[wall_row][wall_col] = 1; //mark wall as visited
+				
+				//mark adjacent node as visited plus add neighboring walls to list
+				if(!(board[wall_row][wall_col - 1])) { //check down
+					board[wall_row][wall_col - 1] = 1; 
+					addWalls(row, col, wall_row, wall_col - 1);
+				} else { //check up
+					board[wall_row][wall_col + 1] = 1;
+					addWalls(row, col, wall_row, wall_col + 1);
+				}
+			} //else printf("double vertical\n");
+		} else {
+			printf("%d %d\n", wall_row, wall_col);
+		}
+
+		//printf("%d %s\n", rand_wall, walls.at(rand_wall).c_str());
+
+		walls.erase(walls.begin() + rand_wall); //remove it from list
+	}
 }
 
 //helper to draw board
@@ -42,10 +125,9 @@ void drawBoard(int **board, int row, int col) {
 	int color;
 	for (int i = 0; i < row; i++) { 
 		for (int j = 0; j < col; j++) {
-			color = board[i][j];
+			color = board[i][j] ^ 1; //^ 1 to flip color
 			printf("newline poly color %1$d %1$d %1$d \
 					pfill %1$d\n", color);
-			//printf("newline poly pfill 0\n");
 			printf("  pts %d %d  %d %d  %d %d  %d %d \n",
 					  i, j, i+1, j, i+1, j+1, i, j+1);
 		}
@@ -69,7 +151,7 @@ void deleteBoard(int **board, int row) {
 }
 //end helpers
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {	
 	//TODO
 	//take user argument to solve maze or not?
 	if(argc < 3) {
@@ -81,19 +163,14 @@ int main(int argc, char *argv[]) {
 	row = stoi(argv[1], NULL);
 	col = stoi(argv[2], NULL);
 
+	srand(time(NULL)); //testing
+
 	//offset for walls
 	row += row - 1;
 	col += col - 1;
 	
 	//allocate space for board
 	board = newBoard(row, col);
-
-	//initialize default values
-	for(int i = 0; i < row; i++) {
-		for(int j = 0; j < col; j++) {
-			board[i][j] = 0;
-		}
-	}
 
 	//create the maze
 	makeMaze(board, row, col);
